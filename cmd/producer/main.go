85a5ba6329d63107a7d1e3dbbb045b9a559d2e3a
bruno.gluszczuk <brunogluszczukjobs@gmail.com>
2025-07-07 09:01:38 -0300

    feat: sync delivery report

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"fmt"
 	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	PublishMessage("Hello Kafka", "test-topic", producer, []byte("key1"))
-	producer.Flush(1000) // Wait for messages to be delivered
-	producer.Close()
+	PublishMessage("Hello Kafka", "test-topic", producer, []byte("key1"), deliveryChan)
+
+	event := <-deliveryChan
+	msg := event.(*kafka.Message)
+	if msg.TopicPartition.Error != nil {
+		fmt.Printf("Failed to deliver message: %v\n", msg.TopicPartition.Error)
+	} else {
+		fmt.Printf("Message delivered to %s [%d] at offset %d\n",
+			*msg.TopicPartition.Topic,
+			msg.TopicPartition.Partition,
+			msg.TopicPartition.Offset)
+	}
+	close(deliveryChan)
 }
 
 func NewKafkaProducer() *kafka.Producer{
@@ -24,7 +37,7 @@ func NewKafkaProducer() *kafka.Producer{
 	return producer
 }
 
-func PublishMessage(msg string, topic string, producer *kafka.Producer, key []byte) error{
+func PublishMessage(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error{
 	message := &kafka.Message{
 		Value: []byte(msg),
 		TopicPartition: kafka.TopicPartition{
@@ -33,7 +46,7 @@ func PublishMessage(msg string, topic string, producer *kafka.Producer, key []by
 		},
 		Key: key,
 	}
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
